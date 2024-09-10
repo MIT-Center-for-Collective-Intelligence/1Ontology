d9a6a7f195d669268cb57cf3122f8dc0ed11b5e2
Haroon Waheed <haroonwaheed21@gmail.com>
2024-09-10 17:20:09 +0500

    add chat section in right side panel

---
+++ src/client/firestore/messages.firestore.ts
@@ -0,0 +1,74 @@
+import { IChat } from " @components/types/IChat";
+import {
+  collection,
+  Firestore,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  startAfter,
+  Unsubscribe,
+  where,
+} from "firebase/firestore";
+
+export type SnapshotChangesTypes = "added" | "modified" | "removed";
+
+export type chatChange = {
+  data: IChat & { id: string };
+  type: SnapshotChangesTypes;
+};
+
+export const getMessagesSnapshot = (
+  db: Firestore,
+  data: {
+    lastVisible: any;
+    nodeId?: string;
+    type: string;
+  },
+  callback: (changes: chatChange[]) => void
+): Unsubscribe => {
+  const { nodeId, type, lastVisible } = data;
+  const pageSize = 15;
+
+  const messagesRef = collection(db, "messages");
+
+  let q = query(
+    messagesRef,
+    where("type", "==", type),
+    where("deleted", "==", false)
+  );
+
+  if (type === "node") {
+    q = query(
+      messagesRef,
+      where("nodeId", "==", nodeId),
+      where("deleted", "==", false)
+    );
+  }
+
+  //   if (lastVisible) {
+  //     q = query(
+  //         messagesRef,
+  //       where("refId", "==", refId),
+  //       where("deleted", "==", false),
+  //       orderBy("createdAt", "desc"),
+  //       startAfter(lastVisible),
+  //       limit(pageSize)
+  //     );
+  //   }
+
+  const killSnapshot = onSnapshot(q, (snapshot) => {
+    const docChanges = snapshot.docChanges();
+
+    const actionTrackDocuments: chatChange[] = docChanges.map((change) => {
+      const document = change.doc.data() as IChat;
+      return {
+        type: change.type,
+        data: { ...document, id: change.doc.id },
+        doc: change.doc,
+      };
+    });
+    callback(actionTrackDocuments);
+  });
+  return killSnapshot;
+};
