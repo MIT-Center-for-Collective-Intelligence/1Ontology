85d1f1b295365cdfadce59aac924a3681fba7972
Iman <oneweb@umich.edu>
2024-09-21 16:50:49 -0400

    Wrote some checks in handleDownload to check discrepancies.

--- src/components/Header/AppHeader.tsx
+++ src/components/Header/AppHeader.tsx
@@ -397,81 +397,210 @@ const AppHeader = forwardRef(
     };
 
     const handleDownload = useCallback(async () => {
-      try {
-        const nodesCollection = query(
-          collection(db, NODES),
-          where("deleted", "==", false),
-          where("root", "==", "hn9pGQNxmQe9Xod5MuKK")
-        );
-        const querySnapshot = await getDocs(nodesCollection);
-        let i = 0;
-        for (let nodeDoc of querySnapshot.docs) {
-          const nodeData = nodeDoc.data() as INode;
-          if (nodeData.specializations) {
-            for (let category of Object.keys(nodeData.specializations)) {
-              for (let specialization of nodeData.specializations[category]) {
-                const specNodeRef = doc(
-                  collection(db, NODES),
-                  specialization.id
-                );
-                const specNodeDoc = await getDoc(specNodeRef);
-                if (!specNodeDoc.exists()) {
+      // try {
+      const nodesCollection = query(
+        collection(db, NODES),
+        where("deleted", "==", false),
+        where("root", "==", "hn9pGQNxmQe9Xod5MuKK")
+      );
+      const querySnapshot = await getDocs(nodesCollection);
+      let i = 0;
+      for (let nodeDoc of querySnapshot.docs) {
+        const nodeData = nodeDoc.data() as INode;
+        if (nodeData.specializations) {
+          for (let category of Object.keys(nodeData.specializations)) {
+            for (let specialization of nodeData.specializations[category]) {
+              const specNodeRef = doc(collection(db, NODES), specialization.id);
+              const specNodeDoc = await getDoc(specNodeRef);
+              if (!specNodeDoc.exists()) {
+                console.log("Specialization not found: ", specialization.title);
+              } else {
+                const specNodeData = specNodeDoc.data() as INode;
+                if (specNodeData.title !== specialization.title) {
                   console.log(
-                    "Specialization not found: ",
+                    "Specialization title mismatch: ",
+                    specNodeData.title,
                     specialization.title
                   );
-                } else {
-                  const specNodeData = specNodeDoc.data() as INode;
-                  if (specNodeData.title !== specialization.title) {
-                    console.log(
-                      "Specialization title mismatch: ",
-                      specNodeData.title,
-                      specialization.title
-                    );
-                  }
-                  if (specNodeData.generalizations) {
+                }
+                if (specNodeData.generalizations) {
+                  let generalizationound = false;
+                  for (let categ of Object.keys(nodeData.generalizations)) {
                     if (
-                      !specNodeData.generalizations[nodeDoc.id] ||
-                      !specNodeData.generalizations[nodeDoc.id].length
+                      specNodeData.generalizations[categ]?.findIndex(
+                        (generali) => generali.id === nodeData.id
+                      ) !== -1
                     ) {
-                      console.log(
-                        "Specialization does not have generalization: ",
-                        specNodeData.title,
-                        nodeData.title
-                      );
+                      generalizationound = true;
                     }
-                  } else {
-                    console.log("Specialization has no generalizations: ", {
+                  }
+                  if (!generalizationound) {
+                    console.log("Generalization not found: ", {
                       specialization: specNodeData.title,
                       node: nodeData.title,
                     });
                   }
+                } else {
+                  console.log("Specialization has no generalizations: ", {
+                    specialization: specNodeData.title,
+                    node: nodeData.title,
+                  });
                 }
               }
             }
           }
-          console.log("Node: ", i++);
         }
-
-        const data = querySnapshot.docs.map((doc) =>
-          getStructureForJSON({
-            ...doc.data(),
-          } as INode)
-        );
-
-        const blob = new Blob([JSON.stringify(data, null, 2)], {
-          type: "application/json",
-        });
-
-        const link = document.createElement("a");
-        link.href = URL.createObjectURL(blob);
-        link.download = "nodes-data.json";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      } catch (error) {
-        console.error("Error downloading JSON: ", error);
+        if (nodeData.generalizations) {
+          for (let category of Object.keys(nodeData.generalizations)) {
+            for (let generalization of nodeData.generalizations[category]) {
+              const genNodeRef = doc(collection(db, NODES), generalization.id);
+              const genNodeDoc = await getDoc(genNodeRef);
+              if (!genNodeDoc.exists()) {
+                console.log("Generalization not found: ", generalization.title);
+              } else {
+                const genNodeData = genNodeDoc.data() as INode;
+                if (genNodeData.title !== generalization.title) {
+                  console.log(
+                    "Generalization title mismatch: ",
+                    genNodeData.title,
+                    generalization.title
+                  );
+                }
+                if (genNodeData.specializations) {
+                  let specializationFound = false;
+                  for (let categ of Object.keys(nodeData.specializations)) {
+                    if (
+                      genNodeData.specializations[categ]?.findIndex(
+                        (speciali) => speciali.id === nodeData.id
+                      ) !== -1
+                    ) {
+                      specializationFound = true;
+                    }
+                  }
+                  if (!specializationFound) {
+                    console.log("Specialization not found: ", {
+                      generalization: genNodeData.title,
+                      node: nodeData.title,
+                    });
+                  }
+                } else {
+                  console.log("Generalization has no specializations: ", {
+                    generalization: genNodeData.title,
+                    node: nodeData.title,
+                  });
+                }
+              }
+            }
+          }
+        }
+        if (nodeData.properties.parts) {
+          for (let category of Object.keys(nodeData.properties.parts)) {
+            for (let part of nodeData.properties.parts[category]) {
+              const partNodeRef = doc(collection(db, NODES), part.id);
+              const partNodeDoc = await getDoc(partNodeRef);
+              if (!partNodeDoc.exists()) {
+                console.log("Part not found: ", part.title);
+              } else {
+                const partNodeData = partNodeDoc.data() as INode;
+                if (partNodeData.title !== part.title) {
+                  console.log(
+                    "Part title mismatch: ",
+                    partNodeData.title,
+                    part.title
+                  );
+                }
+                if (partNodeData.properties.isPartOf) {
+                  let isPartOfFound = false;
+                  for (let categ of Object.keys(nodeData.properties.isPartOf)) {
+                    if (
+                      partNodeData.properties.isPartOf[categ].findIndex(
+                        (partOf) => partOf.id === nodeData.id
+                      ) !== -1
+                    ) {
+                      isPartOfFound = true;
+                    }
+                  }
+                  if (!isPartOfFound) {
+                    console.log("IsPartOf not found: ", {
+                      part: partNodeData.title,
+                      node: nodeData.title,
+                    });
+                  }
+                } else {
+                  console.log("Part has no isPartOf: ", {
+                    part: partNodeData.title,
+                    node: nodeData.title,
+                  });
+                }
+              }
+            }
+          }
+        }
+        if (nodeData.properties.isPartOf) {
+          for (let category of Object.keys(nodeData.properties.isPartOf)) {
+            for (let isPartOf of nodeData.properties.isPartOf[category]) {
+              const isPartOfNodeRef = doc(collection(db, NODES), isPartOf.id);
+              const isPartOfNodeDoc = await getDoc(isPartOfNodeRef);
+              if (!isPartOfNodeDoc.exists()) {
+                console.log("IsPartOf not found: ", isPartOf.title);
+              } else {
+                const isPartOfNodeData = isPartOfNodeDoc.data() as INode;
+                if (isPartOfNodeData.title !== isPartOf.title) {
+                  console.log(
+                    "IsPartOf title mismatch: ",
+                    isPartOfNodeData.title,
+                    isPartOf.title
+                  );
+                }
+                if (isPartOfNodeData.properties.parts) {
+                  let partFound = false;
+                  for (let categ of Object.keys(nodeData.properties.parts)) {
+                    if (
+                      isPartOfNodeData.properties.parts[categ].findIndex(
+                        (part) => part.id === nodeData.id
+                      ) !== -1
+                    ) {
+                      partFound = true;
+                    }
+                  }
+                  if (!partFound) {
+                    console.log("Part not found: ", {
+                      isPartOf: isPartOfNodeData.title,
+                      node: nodeData.title,
+                    });
+                  }
+                } else {
+                  console.log("IsPartOf has no parts: ", {
+                    isPartOf: isPartOfNodeData.title,
+                    node: nodeData.title,
+                  });
+                }
+              }
+            }
+          }
+        }
+        console.log("Node: ", i++);
       }
+
+      const data = querySnapshot.docs.map((doc) =>
+        getStructureForJSON({
+          ...doc.data(),
+        } as INode)
+      );
+
+      const blob = new Blob([JSON.stringify(data, null, 2)], {
+        type: "application/json",
+      });
+
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = "nodes-data.json";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      // } catch (error) {
+      //   console.error("Error downloading JSON: ", error);
+      // }
     }, [db, user]);
 
     return (
