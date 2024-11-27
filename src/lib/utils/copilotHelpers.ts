c40a54b78fa1c85a7ddaf43dee53bb75d4d5fece
Sam ouhra <ouhrac@gmail.com>
2024-11-27 16:52:16 +0100

    udpate the copilot helpers to filter out improvements that do not suggest any changes

--- src/lib/utils/copilotHelpers.ts
+++ src/lib/utils/copilotHelpers.ts
@@ -26,7 +26,12 @@ export const getChangeComparison = ({
       const addedCollections = [];
       const specializations = nodeData[modifiedProperty as "specializations"];
       const _collections = specializations.flatMap((c) => c.collectionName);
+      const previousState = specializations
+        .flatMap((c) => c.nodes)
+        .map((n) => n.id);
+
       const nodesToRemove = new Set();
+      let modified = false;
       for (let collection of change.new_value) {
         if (!_collections.includes(collection.collectionName)) {
           addedCollections.push(collection.collectionName);
@@ -40,8 +45,12 @@ export const getChangeComparison = ({
             addedNonExistentElements.push(title);
             continue;
           }
-          if (collection.collection_changes.nodes_to_add.includes(title)) {
+          if (
+            collection.collection_changes.nodes_to_add.includes(title) &&
+            !previousState.includes(nodeId)
+          ) {
             nodes.push({ id: nodeId, change: "added" });
+            modified = true;
           } else {
             nodes.push({ id: nodeId });
           }
@@ -49,9 +58,10 @@ export const getChangeComparison = ({
         }
         for (let title of collection.collection_changes.nodes_to_delete) {
           const nodeId = nodesByTitle[title].id;
-          if (nodeId) {
+          if (nodeId && previousState.includes(nodeId)) {
             nodes.push({ id: nodeId, change: "removed" });
             nodesToRemove.add(nodeId);
+            modified = true;
           }
         }
 
@@ -63,6 +73,9 @@ export const getChangeComparison = ({
           collectionName: collection.collectionName,
           nodes: final_nodes,
         });
+      }
+      if (!modified) {
+        return;
       }
       return {
         result,
@@ -86,13 +99,26 @@ export const getChangeComparison = ({
       const nodes = [];
       const final_nodes = [];
       const nodesToRemove = new Set();
+      const previousState = (
+        modifiedProperty === "generalizations"
+          ? nodeData[modifiedProperty as "generalizations"]
+          : nodeData.properties[modifiedProperty]
+      )
+        .flatMap((c: any) => c.nodes)
+        .map((n: any) => n.id);
+
+      let modified = false;
       for (let title of newValue.final_array) {
         const nodeId = nodesByTitle[title].id;
         if (!nodeId) {
           addedNonExistentElements.push(title);
           continue;
         }
-        if (newValue.nodes_to_add.includes(title)) {
+        if (
+          newValue.nodes_to_add.includes(title) &&
+          !previousState.includes(nodeId)
+        ) {
+          modified = true;
           nodes.push({ id: nodeId, change: "added" });
         } else {
           nodes.push({ id: nodeId });
@@ -103,7 +129,8 @@ export const getChangeComparison = ({
       }
       for (let title of newValue.nodes_to_delete) {
         const nodeId = nodesByTitle[title].id;
-        if (nodeId) {
+        if (nodeId && !previousState.includes(nodeId)) {
+          modified = true;
           nodes.push({ id: nodeId, change: "removed" });
           nodesToRemove.add(nodeId);
         }
@@ -116,6 +143,7 @@ export const getChangeComparison = ({
         collectionName: "main",
         nodes: final_nodes,
       });
+      if (!modified) return null;
       return {
         result,
         final_result,
@@ -169,8 +197,17 @@ export const getChangeComparison = ({
     }
 
     return null;
-  } catch (error) {
+  } catch (error: any) {
     console.error(error);
+    recordLogs({
+      type: "error",
+      error: JSON.stringify({
+        name: error.name,
+        message: error.message,
+        stack: error.stack,
+      }),
+      at: "getChangeComparison",
+    });
   }
 };
 
@@ -178,76 +215,103 @@ export const compareImprovement = (
   improvement: any,
   nodesByTitle: { [nodeTitle: string]: INode }
 ) => {
-  const _improvement = JSON.parse(JSON.stringify(improvement));
-  const nodeData = nodesByTitle[improvement.title];
-  if (nodeData) {
-    const change = _improvement.change;
-    const modifiedProperty = change.modified_property;
-    const propertyType = nodeData.propertyType[modifiedProperty];
-    const response: any = getChangeComparison({
-      change,
-      nodeData,
-      nodesByTitle,
-    });
-    let propertyValue: any = null;
-    if (
-      modifiedProperty === "specializations" ||
-      modifiedProperty === "generalizations" ||
-      modifiedProperty === "title"
-    ) {
-      propertyValue =
-        nodeData[
-          modifiedProperty as "specializations" | "generalizations" | "title"
-        ];
-    } else {
-      propertyValue = nodeData.properties[modifiedProperty];
-    }
+  try {
+    const _improvement = JSON.parse(JSON.stringify(improvement));
+    const nodeData = nodesByTitle[improvement.title];
+    if (nodeData) {
+      const change = _improvement.change;
+      const modifiedProperty = change.modified_property;
+      const propertyType = nodeData.propertyType[modifiedProperty];
+      const response: any = getChangeComparison({
+        change,
+        nodeData,
+        nodesByTitle,
+      });
+      let propertyValue: any = null;
+      if (
+        modifiedProperty === "specializations" ||
+        modifiedProperty === "generalizations" ||
+        modifiedProperty === "title"
+      ) {
+        propertyValue =
+          nodeData[
+            modifiedProperty as "specializations" | "generalizations" | "title"
+          ];
+      } else {
+        propertyValue = nodeData.properties[modifiedProperty];
+      }
 
-    console.log("response==>", response);
-    if (!!response) {
-      if (propertyType !== "string" && propertyType !== "string-array") {
-        _improvement.detailsOfChange = {
-          comparison: response.result,
-          newValue: response.final_result,
-          previousValue: propertyValue,
-          addedNonExistentElements: response.addedNonExistentElements,
-          addedCollections: response.addedCollections,
-        };
-      } else if (propertyType === "string" || propertyType === "string-array") {
-        _improvement.detailsOfChange = response.changeDetails;
+      if (!!response) {
+        if (propertyType !== "string" && propertyType !== "string-array") {
+          _improvement.detailsOfChange = {
+            comparison: response.result,
+            newValue: response.final_result,
+            previousValue: propertyValue,
+            addedNonExistentElements: response.addedNonExistentElements,
+            addedCollections: response.addedCollections,
+          };
+        } else if (
+          propertyType === "string" ||
+          propertyType === "string-array"
+        ) {
+          _improvement.detailsOfChange = response.changeDetails;
+        }
+      }
+      _improvement.nodeId = nodeData.id;
+      _improvement.modifiedProperty = modifiedProperty;
+      if (modifiedProperty !== "specializations") {
+        _improvement.modiPropertyType = propertyType;
       }
     }
-    _improvement.nodeId = nodeData.id;
-    _improvement.modifiedProperty = modifiedProperty;
-    if (modifiedProperty !== "specializations") {
-      _improvement.modiPropertyType = propertyType;
-    }
+    return _improvement;
+  } catch (error: any) {
+    console.error(error);
+    recordLogs({
+      type: "error",
+      error: JSON.stringify({
+        name: error.name,
+        message: error.message,
+        stack: error.stack,
+      }),
+      at: "getChangeComparison",
+    });
   }
-  return _improvement;
 };
 
 export const filterProposals = (
   improvements: Improvement[],
   nodesByTitle: { [title: string]: INode }
 ) => {
-  const _improvements = [];
+  try {
+    const _improvements = [];
 
-  for (let impv of improvements) {
-    const change = impv.change;
-    const nodeData = nodesByTitle[impv.title];
+    for (let impv of improvements) {
+      const change = impv.change;
+      const nodeData = nodesByTitle[impv.title];
 
-    if (change && nodeData) {
-      const response: any = getChangeComparison({
-        change,
-        nodeData,
-        nodesByTitle,
-      });
+      if (change && nodeData) {
+        const response: any = getChangeComparison({
+          change,
+          nodeData,
+          nodesByTitle,
+        });
 
-      if (!!response) {
-        _improvements.push(impv);
+        if (!!response) {
+          _improvements.push(impv);
+        }
       }
     }
-  }
 
-  return _improvements;
+    return _improvements;
+  } catch (error: any) {
+    recordLogs({
+      type: "error",
+      error: JSON.stringify({
+        name: error.name,
+        message: error.message,
+        stack: error.stack,
+      }),
+      at: "getChangeComparison",
+    });
+  }
 };
