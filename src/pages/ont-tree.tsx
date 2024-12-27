e26a04f59b9899d436f6b7344d65507af1c2cbb6
Sam ouhra <ouhrac@gmail.com>
2024-12-27 17:46:34 +0100

    update the Onet tree view

--- src/pages/ont-tree.tsx
+++ src/pages/ont-tree.tsx
@@ -1,50 +1,137 @@
-import { NODES_ONET } from ' @components/lib/firestoreClient/collections';
-import { collection, getDocs, getFirestore } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
-import { TreeView, TreeItem } from '@mui/lab';
-import { ExpandMore, ChevronRight } from '@mui/icons-material';
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { NODES_ONET } from " @components/lib/firestoreClient/collections";
+import { collection, getDocs, getFirestore, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { TreeItem } from "@mui/lab";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { INode, TreeData } from " @components/types/INode";
+import DraggableTree from " @components/components/OntologyComponents/DraggableTree";
+import { TreeApi } from "react-arborist";
 
 type TreeNode = {
   title: string;
   children: TreeNode[];
 };
 
-const buildTree = (
-  data: { title: string; children: string[] }[]
-): TreeNode[] => {
-  const titleMap: { [key: string]: TreeNode } = {};
+const getTreeView = (
+  mainCategories: INode[],
+  visited: Map<string, any> = new Map(),
+  nodes: any,
+  parentId?: string,
+): any => {
+  const newNodes = [];
+  for (let node of mainCategories) {
+    if (!node) {
+      continue;
+    }
 
-  data.forEach(({ title }) => {
-    titleMap[title] = { title, children: [] };
-  });
-
-  data.forEach(({ title, children }) => {
-    children.forEach((child) => {
-      if (!titleMap[child]) {
-        titleMap[child] = { title: child, children: [] };
+    const specializations = node.specializations;
+    let collections = [];
+    let mainChildren = [];
+    for (let collection of specializations) {
+      const children = [];
+      for (let _node of collection.nodes) {
+        if (nodes[_node.id]) {
+          children.push(nodes[_node.id]);
+        }
       }
-      titleMap[title].children.push(titleMap[child]);
+
+      // if (children.length > 0) {
+      if (collection.collectionName !== "main") {
+        const id = parentId
+          ? `${parentId}-${node.id}-${collection.collectionName}`
+          : `${node.id}-${collection.collectionName}`;
+        if (visited.has(id)) {
+          continue;
+        }
+        visited.set(id, true);
+
+        collections.push({
+          id: id,
+          nodeId: node.id,
+          nodeType: node.nodeType,
+          name: collection.collectionName,
+          children: getTreeView(
+            children,
+            visited,
+            nodes,
+            !node.category ? node.id : undefined,
+          ),
+
+          category: true,
+        });
+      } else {
+        mainChildren.push(...children);
+      }
+
+      // } else {
+      //   collections.push({
+      //     id: `${parentId}-${node.id}`,
+      //     nodeId: node.id,
+      //     name: node.title,
+      //     category: !!parentId
+      //   });
+      // }
+    }
+    const id = parentId ? `${parentId}-${node.id}` : `${node.id}`;
+    if (visited.has(id)) {
+      continue;
+    }
+    visited.set(id, true);
+    newNodes.push({
+      id,
+      nodeId: node.id,
+      name: node.title,
+      nodeType: node.nodeType,
+      children: [
+        ...collections,
+        ...getTreeView(
+          mainChildren,
+          visited,
+          nodes,
+          !node.category ? node.id : undefined,
+        ),
+      ],
+      category: !!node.category,
     });
+  }
+  return newNodes;
+};
+
+const buildTree = (data: any[], nodes: any): TreeNode[] => {
+  const mainCategories = Object.values(data).filter((node: INode) => node.root);
+
+  // Sort main nodes based on a predefined order
+  mainCategories.sort((nodeA: any, nodeB: any) => {
+    const order = [
+      "Information Input",
+      "Interacting With Others",
+      "Mental Processes",
+      "Work Output",
+    ];
+    const nodeATitle = nodeA.title;
+    const nodeBTitle = nodeB.title;
+    return order.indexOf(nodeATitle) - order.indexOf(nodeBTitle);
   });
 
-  const childTitles = new Set(data.flatMap(({ children }) => children));
-  return Object.values(titleMap).filter(({ title }) => !childTitles.has(title));
+  return getTreeView(mainCategories, new Map(), nodes);
 };
 
 function OntTree() {
   const db = getFirestore();
   const [treeData, setTreeData] = useState<TreeNode[]>([]);
   const [loading, setLoading] = useState(false);
+  const [tree, setTree] = useState<TreeApi<TreeData> | null | undefined>(null);
 
   const getData = async () => {
     setLoading(true);
-    const nodesDocs = await getDocs(collection(db, NODES_ONET));
+    const nodesDocs = await getDocs(query(collection(db, NODES_ONET)));
     const nodes_data: any = [];
+    const nodes: any = {};
     nodesDocs.docs.forEach((doc) => {
       nodes_data.push(doc.data());
+      nodes[doc.id] = doc.data();
     });
-    setTreeData(buildTree(nodes_data));
+    setTreeData(buildTree(nodes_data, nodes));
     setLoading(false);
   };
 
@@ -52,64 +139,81 @@ function OntTree() {
     getData();
   }, []);
 
+  const handleDragStart = (
+    event: React.DragEvent<HTMLDivElement>,
+    node: TreeNode,
+  ) => {
+    event.dataTransfer.setData("application/json", JSON.stringify(node));
+  };
+
   const renderTree = (
     nodes: TreeNode,
-    visitedNodes: Set<string> = new Set()
+    visitedNodes: Set<string> = new Set(),
   ) => {
     if (visitedNodes.has(nodes.title)) {
-      return null; // Avoid infinite recursion for circular references
+      return null;
     }
 
     visitedNodes.add(nodes.title);
     return (
-      <TreeItem key={nodes.title} nodeId={nodes.title} label={nodes.title}>
+      <TreeItem
+        key={nodes.title}
+        nodeId={nodes.title}
+        label={
+          <div
+            draggable
+            onDragStart={(event) => handleDragStart(event, nodes)}
+            style={{ cursor: "grab" }}
+          >
+            {nodes.title}
+          </div>
+        }
+      >
         {nodes.children.map((child) =>
-          renderTree(child, new Set(visitedNodes))
+          renderTree(child, new Set(visitedNodes)),
         )}
       </TreeItem>
     );
   };
+  console.log("treeData ==>", treeData);
   if (loading) {
     return (
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-          flexDirection: 'column'
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          flexDirection: "column",
         }}
       >
         <CircularProgress />
-        {/* <br /> */}
-        <Typography sx={{ mt: '5px' }}> Loading...</Typography>
+        <Typography sx={{ mt: "5px" }}> Loading...</Typography>
       </Box>
     );
   }
+
   return (
-    <Box>
-      <Typography
-        sx={{
-          alignItems: 'center',
-          textAlign: 'center',
-          fontSize: '35px',
-          position: 'sticky',
-          top: 0,
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'dark' ? '#28282a' : 'white',
-          zIndex: 5
-        }}
-      >
-        ONet
-      </Typography>
-      <TreeView
-        defaultCollapseIcon={<ExpandMore />}
-        defaultExpandIcon={<ChevronRight />}
-      >
-        {treeData.map((node) => renderTree(node))}
-      </TreeView>
+    <Box sx={{ width: "100%" }}>
+      <DraggableTree
+        treeViewData={treeData}
+        setSnackbarMessage={() => {}}
+        expandedNodes={new Set()}
+        currentVisibleNode={null}
+        nodes={{}}
+        onOpenNodesTree={() => {}}
+        tree={tree}
+        setTree={setTree}
+        treeType="oNet"
+      />
     </Box>
   );
 }
 
-export default OntTree;
+export default function App() {
+  return (
+    <Box sx={{ display: "flex", gap: "16px", padding: "16px" }}>
+      <OntTree />
+    </Box>
+  );
+}
