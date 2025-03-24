b30ebe62a63d36ed59ee22076bb35973a0078fa3
Sam ouhra <ouhrac@gmail.com>
2025-03-24 17:12:54 +0000

    modify the tooltip for the active users

--- src/lib/utils/string.utils.ts
+++ src/lib/utils/string.utils.ts
@@ -127,20 +127,26 @@ export const getPropertyValue = (
 
 export const timeAgo = (timestamp: Timestamp) => {
   const now = new Date();
-  const timeDifference = now.getTime() - timestamp.toMillis(); // Difference in milliseconds
+  const timeDifference = now.getTime() - timestamp.toMillis();
 
   const seconds = Math.floor(timeDifference / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
 
-  if (days > 0) {
+  if (years > 0) {
+    return years === 1 ? "1 year ago" : `${years} years ago`;
+  } else if (months > 0) {
+    return months === 1 ? "1 month ago" : `${months} months ago`;
+  } else if (days > 0) {
     return days === 1 ? "1 day ago" : `${days} days ago`;
   } else if (hours > 0) {
     return hours === 1 ? "1 hour ago" : `${hours} hours ago`;
   } else if (minutes > 0) {
     return minutes === 1 ? "1 minute ago" : `${minutes} minutes ago`;
-  } else if (seconds > 0) {
+  } else {
     return "a few seconds ago";
   }
 };
