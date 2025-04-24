af179908caf99b8d0704cb3f7e12ec071836b974
Soe Min <tsoominn@gmail.com>
2025-04-24 16:52:03 +0800

    Fix title duplication warning behavior

--- src/pages/Ontology.tsx
+++ src/pages/Ontology.tsx
@@ -349,7 +349,7 @@ const Ontology = ({ skillsFuture = false }: { skillsFuture: boolean }) => {
       resizer.resizeSection(2, { toSize: 0 });
       controller.applyResizer(resizer);
     }
-  }, [user, nodes]);
+  }, [user]); // Removed nodes dependency to fix column auto-resizing when node content changes (when editing nodes through yjsEditor)
 
   useEffect(() => {
     const checkIfDifferentDay = () => {
