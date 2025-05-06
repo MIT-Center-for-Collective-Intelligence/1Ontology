886912c9014ebc1b97450761cf168e8cebaec962
Sam ouhra <ouhrac@gmail.com>
2025-05-06 15:11:22 +0100

    add the bullet point at the end of both Important Points section of the prompt

--- src/lib/utils/copilotPrompts.ts
+++ src/lib/utils/copilotPrompts.ts
@@ -129,142 +129,6 @@ export type Improvement = {
   detailsOfChange?: any;
 };
 
-const PROPOSALS_SCHEMA = `
-# Response Structure:
-'''
-Please carefully generate a JSON object with the following structure:
-{
-   "message": "A string message that you would send to the user in response to their message. This could include your analysis, questions, or explanations regarding the requested changes.",
-   "improvements": [], // An array of improvements to existing nodes.
-   "new_nodes": [] // An array of new nodes. Note that you should not propose a new node if a node with the same meaning already exists in the ontology, even if their titles are different.
-}
-
-# For the "improvements" array:
-Each item should represent an object that proposes an improvement to an existing node. Please structure each object as follows:
-{
-"title": "The current title of the node.",
-"nodeType": "The type of the node, which could be 'activity', 'actor', 'object', 'evaluationDimension', 'incentive', 'reward', or 'context'.",
-"changes": [] // An array of objects, each representing a change to a single property of the node that requires modification.
-
-Each change object should include the necessary fields for the property being changed and a "reasoning" field explaining your reasoning for proposing this change.
-
-- For "title" changes:
-{
-  "modified_property": "title",
-  "new_value": "The improved title of the node.",
-  "reasoning": "Your reasoning for proposing this change to the title of the node."
-},
-
-- For "description" changes:
-{
-  "modified_property": "description",
-  "new_value": "The improved title of the node.",
-  "reasoning": "Your reasoning for proposing this change to the description of the node."
-},
-
-- For "specializations" changes:
-{
-  "specializations": "specializations": [], // "Specializations" of a node are nodes that are specific types of that node. Example: Specializations of the node "Buy" can be classified into the following collections: 1- "Buy what?", including specializations such as "Buy fuel", "Buy parts", "Buy services", and "Buy information". 2- "Buy how?", including specializations such as "Buy online" and "Buy in person". 3- "Buy by whom?" including specializations such as: "Buy by marine operations company". The value of this field should be an array of objects, each representing a collection with the following structure: 
-  {
-    "collectionName": "The title of the collection",
-    "changes": {
-      "nodes_to_add": [An array of titles (as strings) of nodes to add to this collection.],
-      "nodes_to_delete": [An array of titles (as strings) of nodes to remove from this collection.],
-      "final_array": [An array of titles (as strings) representing the final set of nodes in this collection after additions and deletions.]
-    },
-    "reasoning": "Your reasoning for proposing this change to the specializations of the node."
-  }
-},
-
-- For "generalizations" changes:
-{
-  "modified_property": "generalizations",
-  "new_value": {
-    "nodes_to_add": [An array of titles (as strings) of nodes to add to generalizations.],
-    "nodes_to_delete": [An array of titles (as strings) of nodes to remove from the generalizations.],
-    "final_array": [An array of titles (as strings) of nodes representing the final state of the property after additions and deletions.]
-  },
-  "reasoning": "Your reasoning for proposing this change to the specializations of the node."
-  }
-},
-
-- For other array property changes (other than "specializations" and "generalizations"), each change object should have the following properties:
-{
-  "modified_property": "[PROPERTY_NAME]",
-  "new_value": {
-    "nodes_to_add": [An array of nodes to add to the existing property.],
-    "nodes_to_delete": [An array of nodes to remove from the existing property.],
-    "final_array": [An array representing the final state of the property after additions and deletions.]
-  },
-  "reasoning": "Your reasoning for proposing this change to the [PROPERTY_NAME] of the node."
-}
-
-- If "nodeType" is "activity" and the property you want to change is "postConditions" or "preConditions":
-{
-  "modifiedProperty": "[postConditions|preConditions]",
-  "new_value": {
-    "conditions_to_add": [An array of conditions to add to the existing property.],
-    "conditions_to_delete": [An array of conditions to remove from the existing property.],
-    "final_array": [An array representing the final state of the property after additions and deletions.]
-  },
-  "reasoning": "Your reasoning for proposing this change to the [postConditions|preConditions] of the node."
-}
-}
-
-IMPORTANT: Please do not propose the creation of any new node under "specializations" or "generalizations" in an improvement object. New nodes should only be proposed under the "new_nodes" array.
-
-For the "new_nodes" array:
-Each item should represent an object proposing a new node. Please structure each object as follows:
-{
-  "title": "The title of the new node.",
-  "description": "The description of the node.",
-  "nodeType": "The type of the node, which could be 'activity', 'actor', 'object', 'evaluationDimension', 'incentive', 'reward', or 'context'.",
-  "generalizations": [An array of titles (as strings) of nodes that are generalizations of this node. These are nodes that this node is a specific type of. Example, "But" is a generalization of nodes such as "Buy fuel", "Buy parts", "Buy services", and "Buy information".],
-  "parts": [An array of titles (as strings) of nodes that are parts of this node.],
-  "isPartOf": [An array of titles (as strings) of nodes that this node is a part of.],
-
-  // NodeType-specific fields:
-
-  // If "nodeType" is "activity":
-  "actor": [An array of titles (as strings) of nodes that are individuals or groups that perform this activity.],
-  "objectsActedOn": [An array of titles (as strings) of nodes that are objects that this activity is performed on.],
-  "evaluationDimension": [An array of titles (as strings) of nodes that are evaluation dimensions of this activity.],
-  "postConditions": [An array of conditions that must be met after this activity is performed.],
-  "preConditions": [An array of conditions that must be met before this activity can be performed.],
-
-  // If "nodeType" is "object":
-  "lifeSpan": [Details about the lifespan of the object.],
-  "modifiability": [Details about the modifiability of the object.],
-  "perceivableProperties": [An array of perceivable properties of the object.],
-
-  // If "nodeType" is "evaluationDimension":
-  "criteriaForAcceptability": [An array of standards used to determine if the activity's performance meets expectations according to this evaluation dimension.],
-  "directionOfDesirability": [An array indicating whether an increase or decrease in measurement is considered desirable.],
-  "evaluationType": [An array of evaluation types.],
-  "measurementUnits": [An array of units used to quantify this evaluation dimension.],
-
-  // If "nodeType" is "reward":
-  "units": [An array of units of the reward.],
-
-  // If "nodeType" is "incentive":
-  "capabilitiesRequired": [An array of capabilities that actors must possess to achieve this incentive.],
-  "rewardFunction": [An array of descriptions of how the reward is structured or provided.],
-  "evaluationDimension": [An array of titles (as strings) of nodes that are evaluation dimensions of the incentive.],
-  "reward": [An array of titles (as strings) of nodes that are rewards of the incentive.],
-  "reasoning": "Your reasoning for proposing this new node with these specific properties."
-}
-
-### IMPORTANT NOTES:
-- Please do not propose any new node that already exists in the ontology, even if their titles are different. Ensure that each node is unique in meaning.
-- Take as much time as needed to generate as many high-quality improvements and new nodes as possible.
-- Thoroughly analyze the ontology and the user's message to identify all possible improvements and additions.
-- If a 'Main' collection does not exist, please do not create it.
-- Please note the difference between "specializations" and "parts" properties.
-  - "specializations" is an array of specialized nodes that are specific types of this node.
-  - "parts" is an array of smaller nodes that are components of this node.
-'''
-`;
-
 export const MODELS_OPTIONS = [
   { id: "o3", title: "O3" },
   { id: "o1", title: "O1" },
@@ -687,7 +551,8 @@ Each change object should include the necessary fields for the property being ch
   - The changes to be made (additions, deletions, final state).
   - A **reasoning** field explaining why you are proposing these changes.
 - For properties that are single values (like **lifeSpan**, **modifiability**, **directionOfDesirability**, **numberOfIndividualsInGroup**), provide the new value directly along with the reasoning.
-- Do not propose creating new nodes within "specializations" or "generalizations" in an improvement object. New nodes should only be proposed under the "new_nodes" array.`);
+- Do not propose creating new nodes within "specializations" or "generalizations" in an improvement object. New nodes should only be proposed under the "new_nodes" array.
+- In the title of the parts, do not mention that they are inherited from another node. We do not need any comments about whether and where each part is inherited from.`);
   }
 
   return sections.join("\n\n");
@@ -729,7 +594,8 @@ export const getNotesPrompt = () => {
   return `
 - Do not create a 'Main' collection if it doesn't exist.
 - Take ample time to generate high-quality improvements and additions.
-- A panel of experts will review your proposals and for every satisfactory proposal, you will be rewarded $100. For every unsatisfactory proposal, you will lose $100.`;
+- A panel of experts will review your proposals and for every satisfactory proposal, you will be rewarded $100. For every unsatisfactory proposal, you will lose $100.
+- In the title of the parts, do not mention that they are inherited from another node. We do not need any comments about whether and where each part is inherited from.`;
 };
 
 export const getCopilotPrompt = ({
