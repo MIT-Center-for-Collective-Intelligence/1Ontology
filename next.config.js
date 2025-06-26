8afaa6267a9255789f833854612be76706617cab
Sam ouhra <ouhrac@gmail.com>
2025-06-26 18:51:11 +0100

    refactor: redirect the route SkillsFuture  to root

--- next.config.js
+++ next.config.js
@@ -12,4 +12,13 @@ module.exports = {
 
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: "/SkillsFuture/:id*",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
 };
