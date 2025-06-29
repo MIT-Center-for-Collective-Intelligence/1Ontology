d9e8f98ef77de7958aeab7deacf36adfc9193b28
Sam ouhra <ouhrac@gmail.com>
2025-06-29 15:20:16 +0100

    update the generalizations desplayed title for it

--- src/lib/CONSTANTS.ts
+++ src/lib/CONSTANTS.ts
@@ -29,7 +29,7 @@ export const DISPLAY: {
   actor: "Actors",
   objectsActedOn: "Objects Acted On",
   ONetID: "O*Net ID",
-  generalizations: 'Generalizations ("Parents"',
+  generalizations: 'Generalizations ("Parents")',
   specializations: 'Specializations ("Children")',
 };
 
