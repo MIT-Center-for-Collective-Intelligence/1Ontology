15ed696f5627c5fbc04ece29d652e24eab0021bc
Sam ouhra <ouhrac@gmail.com>
2025-07-07 02:05:55 +0100

    call searchChromaExtension endpoint for search instead of searchChroma

--- src/components/SearchSideBar/SearchSideBar.tsx
+++ src/components/SearchSideBar/SearchSideBar.tsx
@@ -137,7 +137,7 @@ const SearchSideBar = ({
       setErrorSearch(false);
       setLoadingSearchResult(true);
       const response: any = !development
-        ? await Post("/searchChroma", {
+        ? await Post("/searchChromaExtension", {
             query: searchValue,
             skillsFuture,
             appName: skillsFuture ? skillsFutureApp : null,
