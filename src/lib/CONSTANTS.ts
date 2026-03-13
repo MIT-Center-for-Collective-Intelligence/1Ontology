96f686e7b14e5efa976ed1963dc6a3fc4b9c13c1
Sam ouhra <ouhrac@gmail.com>
2026-03-13 19:26:50 +0000

    add a new ontology called Noun Ontology

--- src/lib/CONSTANTS.ts
+++ src/lib/CONSTANTS.ts
@@ -198,7 +198,7 @@ export const ONTOLOGY_APPS = [
     id: "final-hierarchy-with-o*net-parts-inheritance",
     name: "Final Hierarchy with O*Net Parts with inheritance",
   },
-
+  { id: "noun-ontology", name: "Noun Ontology" },
   {
     id: "xinru",
     name: "Xinru",
